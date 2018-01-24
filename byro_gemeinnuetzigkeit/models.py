1759bba7863cb9b879a282cfae050b948559b0ea
Tobias Kunze <rixx@cutebit.de>
2018-01-24 17:20:04 +0100

    Add configuration model

---
+++ byro_gemeinnuetzigkeit/models.py
@@ -0,0 +1,29 @@
+from django.conf.global_settings import LANGUAGES
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from solo.models import SingletonModel
+
+
+class GemeinnuetzigkeitConfiguration(SingletonModel):
+
+    finanzamt = models.CharField(
+        null=True, blank=True,
+        max_length=300,
+        verbose_name=_('Finanzamt'),
+    )
+    vat_id = models.CharField(
+        null=True, blank=True,
+        max_length=50,
+        verbose_name=_('VAT-ID'),
+    )
+    reason = models.CharField(
+        null=True, blank=True,
+        max_length=50,
+        verbose_name=_('reason for reduced taxes'),
+    )
+    notification_date = models.CharField(
+        null=True, blank=True,
+        max_length=50,
+        verbose_name=_('last date of notification'),
+    )
